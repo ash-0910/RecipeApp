2e4c158d65b14888c563eab0e6b5d13682741192
ash-0910 <A00244471@mycambrian.ca>
2022-04-06 14:40:22 -0400

    Add project files.

---
+++ RecipeApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RecipeApp.Areas.Identity.Data;
+using RecipeApp.Data;
+
+[assembly: HostingStartup(typeof(RecipeApp.Areas.Identity.IdentityHostingStartup))]
+namespace RecipeApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<RecipeAppContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("RecipeAppContextConnection")));
+
+                services.AddDefaultIdentity<RecipeAppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<RecipeAppContext>();
+            });
+        }
+    }
+}
